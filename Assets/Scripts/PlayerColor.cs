72a3f60a2b74b229ab43b2ca3e5cc0a1df636285
Hubert <huberthalim7@gmail.com>
2019-05-24 10:49:49 +0800

    Added random player color

---
+++ Assets/Scripts/PlayerColor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerColor : MonoBehaviour {
+
+    public List<Material> mats = new List<Material>();
+    
+    void Awake() {
+
+        GetComponent<Renderer>().material = mats[Random.Range(0, mats.Count)];
+    }
+
+}
