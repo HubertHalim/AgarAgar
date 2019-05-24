f5efe283925a4c9b78b6ea296df929d52829143c
Hubert <huberthalim7@gmail.com>
2019-05-24 13:18:01 +0800

    added food

---
+++ Assets/Scripts/Eat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Eat : MonoBehaviour {
+
+    public string Tag;
+    public float increase;
+
+    void OnTriggerEnter(Collider other) {
+
+        if (other.gameObject.tag == Tag) {
+            transform.localScale += new Vector3(increase, increase, increase);
+            Destroy(other.gameObject);
+        } 
+    }
+ 
+}
---
+++ Assets/Scripts/FoodSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawn : MonoBehaviour {
+
+    public GameObject food;
+    public float speed;
+
+    void Start() {
+
+        InvokeRepeating("Generate", 0, speed);
+    }
+
+    void Generate() {
+
+        int x = Random.Range(0, Camera.main.pixelWidth);
+        int y = Random.Range(0, Camera.main.pixelHeight);
+
+        Vector3 target = Camera.main.ScreenToWorldPoint(new Vector3(x, y, 0));
+        target.z = 0;
+
+        Instantiate(food, target, Quaternion.identity);
+    
+    }
+
+}
