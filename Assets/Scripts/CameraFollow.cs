3e785d6acb2dac69d4571f2a882c5a5a81a65729
JeremyKwok <jeremykke@gmail.com>
2019-05-29 09:05:01 +0800

    Added Camera follow script, buggy

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTank;
+    public float cameraDistance = 30.0f;
+
+    void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = (Screen.height / 2) / cameraDistance;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(playerTank.position.x, playerTank.position.y, playerTank.position.z);
+    }
+    
+}
