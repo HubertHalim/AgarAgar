f2904152299b8420e6f17425ab839b98e1311f60
Hubert <huberthalim7@gmail.com>
2019-06-20 14:29:24 +0800

    enemy created

---
+++ Assets/Scripts/GlobalState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalState : Singleton<GlobalState> {
+
+    protected GlobalState() { }
+
+    public bool alive = true;
+}
