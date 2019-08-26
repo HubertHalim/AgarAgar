d23a8b57b7b21a6cf89f44da7e1c0b8b1dfa44a1
JeremyKwok <jeremykke@gmail.com>
2019-08-26 20:01:25 +0800

    UI added

--- Assets/Scripts/GlobalState.cs
+++ Assets/Scripts/GlobalState.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class GlobalState : Singleton<GlobalState> {
 
     protected GlobalState() { }
-
+    public int highScore;
+    public float difficulty;
     public bool alive = true;
 
     public List<Transform> objects = new List<Transform>();
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager: MonoBehaviour
+{
+    public void StartEasyGame()
+    {
+        GlobalState.Instance.highScore = 0;
+        GlobalState.Instance.difficulty = 0.4f;
+
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void StartMediumGame()
+    {
+        GlobalState.Instance.highScore = 0;
+        GlobalState.Instance.difficulty = 0.5f;
+
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void StartHardGame()
+    {
+        GlobalState.Instance.highScore = 0;
+        GlobalState.Instance.difficulty = 0.9f;
+
+        SceneManager.LoadScene("SampleScene");
+    }
+
+}
